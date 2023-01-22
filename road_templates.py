be772afee04110fb40022b7b33f0fbb8119992fd
Stephen Gadd <42514781+docuracy@users.noreply.github.com>
2023-01-22 14:32:04 +0000

    -

---
+++ road_templates.py
@@ -0,0 +1,95 @@
+import math
+import numpy as np
+import cv2
+from skimage.metrics import structural_similarity
+
+def generate_templates():
+    # Template definitions
+    MAX_ROAD_WIDTH = 15  # Pixel width of road between border lines. Should be an odd number
+    MIN_ROAD_WIDTH = 3 # Should be an odd number
+    ROAD_BORDER_THICKNESS = 4 # Pixel width of lines defining road edges
+    MARGIN = 4 # Typical minimum clear space either side of road
+    ROAD_COLOUR = (255, 255, 204)
+    BORDER_COLOUR = (70, 70, 70)
+    BACKGROUND_COLOUR = (255, 255, 204)
+    
+    template_images = []
+    for road_width in range(MAX_ROAD_WIDTH, MIN_ROAD_WIDTH-2, -2):
+        template_w = (ROAD_BORDER_THICKNESS+MARGIN)*2+road_width
+        # Create a blank template image with the specified background color
+        template_image = np.full((template_w, template_w, 3), BACKGROUND_COLOUR, dtype=np.uint8)
+        template_image = cv2.cvtColor(template_image, cv2.COLOR_BGR2GRAY) 
+        
+        # Draw the line and borders on the template image
+        template_image = cv2.rectangle(template_image, (0, template_w // 2-ROAD_BORDER_THICKNESS-road_width // 2), (template_w, template_w // 2+ROAD_BORDER_THICKNESS+road_width // 2), BORDER_COLOUR, -1)  # borders
+        template_image = cv2.rectangle(template_image, (0, template_w // 2-road_width // 2), (template_w, template_w // 2+road_width // 2), ROAD_COLOUR, -1)  # Line
+        
+        # Mask the template
+        template_images.append(template_image)
+    
+    return template_images
+
+def score_linestrings(linestrings, TEMPLATE_SAMPLE, raster_image_gray):
+    # Test sample points from extracted LineStrings for structural_similarity to roads, 
+    # using templates rotated to the orientation of the LineString at each sample point.
+    # Filter out all LineStrings not meeting minimum similarity threshold. 
+    
+    templates = generate_templates()
+    linestring_attributes = []
+    
+    for k, (linestring) in enumerate(linestrings):
+        print("{}/{}".format(k, len(linestrings)))
+        if (linestring.length < TEMPLATE_SAMPLE):
+            linestring_attributes.append({'score': 0, 'width': 0})
+            continue
+        scores = []
+        widths = []
+        
+        for i in range(0, int(linestring.length)-TEMPLATE_SAMPLE, TEMPLATE_SAMPLE):
+            point1 = linestring.interpolate(i)  # get the Point at distance i along the LineString
+            point2 = linestring.interpolate(i+TEMPLATE_SAMPLE)  # get the Point at distance i+TEMPLATE_SAMPLE along the LineString
+            orientation = math.degrees(math.atan2(point2.y - point1.y, point2.x - point1.x))
+            
+            clip_size = int(templates[0].shape[0] * math.sqrt(2)) # Maximum dimension of rotated templates
+            clip_region = cv2.getRectSubPix(raster_image_gray, (clip_size, clip_size), (point1.x, point1.y), cv2.INTER_CUBIC)
+            rot_matrix = cv2.getRotationMatrix2D((clip_size / 2, clip_size / 2), orientation, 1)
+            clip_region = cv2.warpAffine(clip_region, rot_matrix, (clip_size, clip_size), cv2.INTER_CUBIC)
+            clip_region = cv2.getRectSubPix(clip_region, (templates[0].shape[0],templates[0].shape[0]), (clip_size / 2, clip_size / 2))
+            
+            # print('Region: '+str(templates[0].shape[0]))
+            # cv2.imshow("Clip region", clip_region)
+            # # cv2.imshow("Template", template)
+            # cv2.waitKey(0)
+            # cv2.destroyAllWindows()
+            
+            for j, (template) in enumerate(templates):
+                clip_sample = cv2.getRectSubPix(clip_region, (template.shape[0],template.shape[0]), (clip_region.shape[0] / 2, clip_region.shape[0] / 2))
+                
+                match_score = structural_similarity(template, clip_sample)
+                if np.isnan(match_score):
+                    match_score = -1
+                if j == 0:
+                    scores.append(match_score)
+                    widths.append(template.shape[0])
+                else:
+                    # Score the best match from different templates at this point
+                    if match_score > scores[-1]:
+                        scores[len(scores)-1] = match_score
+                        widths[len(scores)-1] = template.shape[0]
+                # if match_score > .5:
+                #     print('Score: '+str(match_score)+' | Distance: '+str(i * TEMPLATE_SAMPLE)+' | Angle: '+str(orientation)+' | width: '+str(template.shape[0]))
+                #     cv2.imshow("Sample region", clip_sample)
+                #     # cv2.imshow("Template", template)
+                #     cv2.waitKey(0)
+                #     cv2.destroyAllWindows()
+        
+        mean_score = np.mean(scores)
+        mean_width = np.mean(widths)
+        if np.isnan(mean_score) or mean_score == 0:
+            linestring_attributes.append({'score': 0, 'width': 0})
+        else:
+            attributes = {'score': int(mean_score*100), 'width': mean_width}
+            linestring_attributes.append(attributes)
+            # print(linestring.length, attributes)    
+                            
+    return linestring_attributes
