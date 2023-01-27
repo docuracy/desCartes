ef7c69d23702d54b9ef35a8ed95289cc109adec1
Stephen Gadd <42514781+docuracy@users.noreply.github.com>
2023-01-27 17:31:34 +0000

    -

--- patch_linestrings.py
+++ patch_linestrings.py
@@ -1,47 +1,76 @@
 import networkx as nx
-from shapely.geometry import LineString, MultiLineString
+from shapely.geometry import Point, LineString, MultiLineString
+from shapely.ops import linemerge
 import itertools
 from collections import defaultdict
+from pickle import NONE
 
-def merge_linestrings(linestrings, modernity, score, width, gap_size):
+def find_shortest_path(linestrings, modernity, score, width, gap_size, start_point = Point(0,10000), end_point = Point(10000,0)):
     print('Merging: '+modernity[0])
-    # Create a directed graph and add nodes to the graph representing each LineString
+    # Create a directed graph and add nodes to the graph representing each LineString endpoint
     G = nx.DiGraph()
+    # Add nodes and edges for each LineString
     for i, ls in enumerate(linestrings):
-        G.add_node(i, geometry=ls, score=score[i], width=width[i])
-    # Add edges between nodes representing LineStrings that can be merged
-    for i, ls1 in enumerate(linestrings):
-        for j, ls2 in enumerate(linestrings):
-            if i != j:
-                if ls1.distance(ls2) <= gap_size:
-                    G.add_edge(i, j, weight=ls1.length + ls2.length)
-    # Find the longest path through the graph using Dijkstra's algorithm
+        G.add_node(ls.coords[0], pos=ls.coords[0])
+        G.add_node(ls.coords[-1], pos=ls.coords[-1])
+        G.add_edge(ls.coords[0], ls.coords[-1], linestring=ls, weight=ls.length)
+        G.add_edge(ls.coords[-1], ls.coords[0], linestring=LineString(ls.coords[::-1]), weight=ls.length)
+        if modernity[0] == 'id92CC9581-BCCE-4892-8D4B-6D71E531186A':
+            print(ls)
+        # Add edges to bridge shortest gaps between linestring pairs
+        for ls2 in linestrings[i+1:]:
+            closest_distance = float('inf')
+            for endpoint1 in ls.coords[0], ls.coords[-1]:
+                for endpoint2 in ls2.coords[0], ls2.coords[-1]:
+                    distance = Point(endpoint1).distance(Point(endpoint2))
+                    if distance < closest_distance:
+                        closest_distance = distance
+                        bridge_start, bridge_end = endpoint1, endpoint2
+            if closest_distance < 500:
+                G.add_edge(bridge_start, bridge_end, linestring=LineString([bridge_start, bridge_end]), weight=closest_distance*10) # weight to discourage use of gap-bridges
+                G.add_edge(bridge_end, bridge_start, linestring=LineString([bridge_end, bridge_start]), weight=closest_distance*10) # weight to discourage use of gap-bridges
+                if modernity[0] == 'id92CC9581-BCCE-4892-8D4B-6D71E531186A':
+                    print(LineString([bridge_start, bridge_end]))
+                # print('Added edge: '+str(int(closest_distance)))
+            # else:
+                # print('Skipped adding edge: '+str(int(closest_distance)))
+
+    # Find closest endpoint to start_point
+    closest_start = None
+    closest_start_dist = float("inf")
+    for node in G.nodes():
+        dist = start_point.distance(Point(node))
+        if dist < closest_start_dist:
+            closest_start = node
+            closest_start_dist = dist
+    # Find closest endpoint to end_point
+    closest_end = None
+    closest_end_dist = float("inf")
+    for node in G.nodes():
+        dist = end_point.distance(Point(node))
+        if dist < closest_end_dist:
+            closest_end = node
+            closest_end_dist = dist
+            
+    if modernity[0] == 'id92CC9581-BCCE-4892-8D4B-6D71E531186A':
+        print(start_point,closest_start,end_point,closest_end)
+    
+    # Find the shortest path between closest_start and closest_end
     try:
-        print(G)
-        end = len(G)-1
-        pred, dist = nx.dijkstra_predecessor_and_distance(G, 0, weight='weight')
+        path = nx.shortest_path(G, closest_start, closest_end, weight='weight')
     except nx.NetworkXNoPath:
         print('NetworkXNoPath')
-        return None, None
-    # Reconstruct the path from the Dijkstra output
-    node = pred.get(end)
-    if node is None:
-        return None, None
-    path = [end]
-    while node is not None:
-        path.append(node)
-        node = pred.get(node)
-    path = list(reversed(path))
-
-    # Create the new merged LineString by concatenating the coordinates of the LineStrings in the path
-    merged_geometry = LineString( list(itertools.chain.from_iterable(G.nodes[node]['geometry'].coords for node in path)) )  
+        return None, [modernity[0],0,0]
     
-    # Compute the average values of the attributes
-    length = sum(G.nodes[i]['geometry'].length for i in range(len(G)))
-    score = sum(G.nodes[i]['score'] * G.nodes[i]['geometry'].length for i in path) / length
-    width = sum(G.nodes[i]['width'] * G.nodes[i]['geometry'].length for i in path) / length
-    
-    return merged_geometry, [modernity[0], score, width]
+    linestrings = []
+    for i in range(len(path)-1):
+        linestrings.append(G.edges[path[i], path[i+1]]['linestring'])
+    coords = []
+    for ls in linestrings:
+        coords.extend(ls.coords)
+    if len(coords) < 2:
+        return None, [modernity[0],0,0]
+    return LineString(coords), [modernity[0],0,0]
 
 def merge_groups(line_strings, attributes, gap_size):
     new_line_strings = []
@@ -58,12 +87,16 @@ def merge_groups(line_strings, attributes, gap_size):
     
     # Create new sets of LineStrings and attributes based on merged groups
     for group in line_string_groups.values():
-        merged_line_string, merged_attributes = merge_linestrings([ls for ls, attr in group], [attr['modernity'] for ls, attr in group], [attr['score'] for ls, attr in group], [attr['width'] for ls, attr in group], gap_size)
+        # merged_line_string, merged_attributes = merge_linestrings([ls for ls, attr in group], [attr['modernity'] for ls, attr in group], [attr['score'] for ls, attr in group], [attr['width'] for ls, attr in group], gap_size)
+        merged_line_string, merged_attributes = find_shortest_path([ls for ls, attr in group], [attr['modernity'] for ls, attr in group], [attr['score'] for ls, attr in group], [attr['width'] for ls, attr in group], gap_size)
         if merged_line_string is not None:
             new_line_strings.append(merged_line_string)
             keys = ['modernity', 'score', 'width']
             merged_attributes_dict = dict(zip(keys, merged_attributes))
             new_attributes.append(merged_attributes_dict)
+        else:
+            new_line_strings.append(line_strings[0])
+            new_attributes.append(attributes[0])
         
     return new_line_strings, new_attributes
 
