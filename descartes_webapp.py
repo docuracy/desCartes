fc14a74225d0697efc2076bfb14e6a909aa58413
Stephen Gadd <42514781+docuracy@users.noreply.github.com>
2023-02-04 23:43:19 +0000

    -

--- descartes_webapp.py
+++ descartes_webapp.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1 style='color:blue'>desCartes!</h1>"
+    msg = request.args.get('msg')
+    if msg:
+        return "<h1 style='color:blue'>desCartes! Message: {}</h1>".format(msg)
+    else:
+        return "<h1 style='color:blue'>desCartes!</h1>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
