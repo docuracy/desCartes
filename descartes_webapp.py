fc25a9a725ffa156fddae504ed791e872e9a0eba
Stephen Gadd <42514781+docuracy@users.noreply.github.com>
2023-02-05 12:16:51 +0000

    Update descartes_webapp.py

--- descartes_webapp.py
+++ descartes_webapp.py
@@ -4,13 +4,19 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def hello():
     if request.method == 'GET':
-        bounds = request.get_json().get('bounds')
+        bounds = request.args.get('bounds')
         if bounds:
             return jsonify({"bounds": bounds})
         else:
             return jsonify({"message": "No bounds found in the request."})
     else:
-        return jsonify({"message": "POST desCartes!"})
+        # POST requests are not properly handled by nginx/Apache on IONOS system
+        msg = request.get_json().get('msg')
+        if msg:
+            return jsonify({"message": msg})
+        else:
+            return jsonify({"message": "POST desCartes!"})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
+
