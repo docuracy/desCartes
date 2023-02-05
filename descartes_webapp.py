190586436b292c23823519a66e9a2eff171e0961
Stephen Gadd <42514781+docuracy@users.noreply.github.com>
2023-02-05 00:22:06 +0000

    -

--- descartes_webapp.py
+++ descartes_webapp.py
@@ -1,13 +1,13 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     msg = request.args.get('msg')
     if msg:
-        return "<h1 style='color:blue'>desCartes! Message: {}</h1>".format(msg)
+        return jsonify({"message": msg})
     else:
-        return "<h1 style='color:blue'>desCartes!</h1>"
+        return jsonify({"message": "desCartes!"})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
