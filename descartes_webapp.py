98fb7e5f4884554d95fb4a85e3ccdf2a3be2fdc8
Stephen Gadd <42514781+docuracy@users.noreply.github.com>
2023-02-05 15:21:31 +0000

    -

--- descartes_webapp.py
+++ descartes_webapp.py
@@ -36,45 +36,48 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def hello():
-    if request.method == 'GET':
-        bounds = request.args.get('bounds')
-        if bounds:
-            EXTENT = bounds.split(",")
-            EXTENT = [float(x) for x in EXTENT]
-            mapfile = create_geotiff (RASTER_TILE_URL, OUTPUTDIR, GEOTIFF_NAME, EXTENT, RASTER_TILE_ZOOM)
-            
-            # Open the geotiff using rasterio
-            with rasterio.open(mapfile) as raster:
-                raster_image = raster.read()
+    try:
+        if request.method == 'GET':
+            bounds = request.args.get('bounds')
+            if bounds:
+                EXTENT = bounds.split(",")
+                EXTENT = [float(x) for x in EXTENT]
+                mapfile = create_geotiff (RASTER_TILE_URL, OUTPUTDIR, GEOTIFF_NAME, EXTENT, RASTER_TILE_ZOOM)
                 
-            raster_image_gray = cv2.cvtColor(cv2.merge(raster_image[:3]), cv2.COLOR_BGR2GRAY)
-            _, result_binary = cv2.threshold(raster_image_gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-            
-            result_binary = erase_areas(result_binary, MAX_ROAD_WIDTH ** 2, blobs = True, black = True) # Attempts to remove circular markers from roadways on GB OS maps
-            result_binary = erase_matches(raster_image_gray, result_binary, './data/templates', 'tree-broadleaf.png')
-            result_binary = erase_matches(raster_image_gray, result_binary, './data/templates', 'tree-conifer.png', threshold=0.7)
-            result_binary = erase_areas(result_binary, 
-                factor = 2 * MAX_ROAD_WIDTH / MIN_ROAD_WIDTH, 
-                contour_width_max = 3 * MAX_ROAD_WIDTH, 
-                convexity_min = .5, 
-                closed = True,
-                shading = True,
-                thresholds = [.7,.65]
-                )   
-            contours = skeleton_contours(result_binary)
-            raster_image_contours = cv2.cvtColor(raster_image_gray, cv2.COLOR_GRAY2BGR)
-            cv2.drawContours(raster_image_contours, contours, -1, (0,0,255), 3)   
-            encoded_image = base64.b64encode(cv2.imencode('.jpg', raster_image_contours)[1]).decode("utf-8")
-            return jsonify({"skeleton_contours": encoded_image})
+                # Open the geotiff using rasterio
+                with rasterio.open(mapfile) as raster:
+                    raster_image = raster.read()
+                    
+                raster_image_gray = cv2.cvtColor(cv2.merge(raster_image[:3]), cv2.COLOR_BGR2GRAY)
+                _, result_binary = cv2.threshold(raster_image_gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+                
+                result_binary = erase_areas(result_binary, MAX_ROAD_WIDTH ** 2, blobs = True, black = True) # Attempts to remove circular markers from roadways on GB OS maps
+                result_binary = erase_matches(raster_image_gray, result_binary, './data/templates', 'tree-broadleaf.png')
+                result_binary = erase_matches(raster_image_gray, result_binary, './data/templates', 'tree-conifer.png', threshold=0.7)
+                result_binary = erase_areas(result_binary, 
+                    factor = 2 * MAX_ROAD_WIDTH / MIN_ROAD_WIDTH, 
+                    contour_width_max = 3 * MAX_ROAD_WIDTH, 
+                    convexity_min = .5, 
+                    closed = True,
+                    shading = True,
+                    thresholds = [.7,.65]
+                    )   
+                contours = skeleton_contours(result_binary)
+                raster_image_contours = cv2.cvtColor(raster_image_gray, cv2.COLOR_GRAY2BGR)
+                cv2.drawContours(raster_image_contours, contours, -1, (0,0,255), 3)   
+                encoded_image = base64.b64encode(cv2.imencode('.jpg', raster_image_contours)[1]).decode("utf-8")
+                return jsonify({"skeleton_contours": encoded_image})
+            else:
+                return jsonify({"message": "No bounds found in the request."})
         else:
-            return jsonify({"message": "No bounds found in the request."})
-    else:
-        # POST requests are not properly handled by nginx/Apache on IONOS Plesk system
-        msg = request.get_json().get('msg')
-        if msg:
-            return jsonify({"message": msg})
-        else:
-            return jsonify({"message": "POST desCartes!"})
+            # POST requests are not properly handled by nginx/Apache on IONOS Plesk system
+            msg = request.get_json().get('msg')
+            if msg:
+                return jsonify({"message": msg})
+            else:
+                return jsonify({"message": "POST desCartes!"})
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
