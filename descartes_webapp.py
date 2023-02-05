182e776d321840dec1549ba021b9a0195da8e9ab
Stephen Gadd <42514781+docuracy@users.noreply.github.com>
2023-02-05 17:05:00 +0000

    Update descartes_webapp.py

--- descartes_webapp.py
+++ descartes_webapp.py
@@ -52,21 +52,21 @@ def hello():
                 result_binary = erase_areas(result_binary, raster_image_gray, factor = MAX_ROAD_WIDTH ** 2, blobs = True, black = True, SHOW_IMAGES = False, OUTPUTDIR = OUTPUTDIR) # Attempts to remove circular markers from roadways on GB OS maps
                 # result_binary = erase_matches(raster_image_gray, result_binary, './data/templates', 'tree-broadleaf.png', SHOW_IMAGES = False, OUTPUTDIR = OUTPUTDIR)
                 # result_binary = erase_matches(raster_image_gray, result_binary, './data/templates', 'tree-conifer.png', threshold=0.7, SHOW_IMAGES = False, OUTPUTDIR = OUTPUTDIR)
-                # result_binary = erase_areas(result_binary, raster_image_gray, 
-                #     factor = 2 * MAX_ROAD_WIDTH / MIN_ROAD_WIDTH, 
-                #     contour_width_max = 3 * MAX_ROAD_WIDTH, 
-                #     convexity_min = .5, 
-                #     closed = True,
-                #     shading = True,
-                #     thresholds = [.7,.65], 
-                #     SHOW_IMAGES = False, 
-                #     OUTPUTDIR = OUTPUTDIR
-                #     )   
-                # contours = skeleton_contours(result_binary, raster_image_gray, SHOW_IMAGES = False)
-                # raster_image_contours = cv2.cvtColor(raster_image_gray, cv2.COLOR_GRAY2BGR)
-                # cv2.drawContours(raster_image_contours, contours, -1, (0,0,255), 3)   
-                # encoded_image = base64.b64encode(cv2.imencode('.jpg', raster_image_contours)[1]).decode("utf-8")
-                encoded_image = base64.b64encode(cv2.imencode('.jpg', result_binary)[1]).decode("utf-8")
+                result_binary = erase_areas(result_binary, raster_image_gray, 
+                    factor = 2 * MAX_ROAD_WIDTH / MIN_ROAD_WIDTH, 
+                    contour_width_max = 3 * MAX_ROAD_WIDTH, 
+                    convexity_min = .5, 
+                    closed = True,
+                    shading = True,
+                    thresholds = [.7,.65], 
+                    SHOW_IMAGES = False, 
+                    OUTPUTDIR = OUTPUTDIR
+                    )   
+                contours = skeleton_contours(result_binary, raster_image_gray, SHOW_IMAGES = False)
+                raster_image_contours = cv2.cvtColor(raster_image_gray, cv2.COLOR_GRAY2BGR)
+                cv2.drawContours(raster_image_contours, contours, -1, (0,0,255), 3)   
+                encoded_image = base64.b64encode(cv2.imencode('.jpg', raster_image_contours)[1]).decode("utf-8")
+                # encoded_image = base64.b64encode(cv2.imencode('.jpg', result_binary)[1]).decode("utf-8")
                 return jsonify({"base64_image": encoded_image})
             else:
                 return jsonify({"message": "No bounds found in the request."})
