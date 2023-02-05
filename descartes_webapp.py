66d31aa178b6b2f97ecd77930a6224e62049e2c3
Stephen Gadd <42514781+docuracy@users.noreply.github.com>
2023-02-05 12:10:41 +0000

    Update descartes_webapp.py

--- descartes_webapp.py
+++ descartes_webapp.py
@@ -1,13 +1,16 @@
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-@app.route("/", methods=['GET'])
+@app.route("/", methods=['GET', 'POST'])
 def hello():
-    bounds = request.get_json().get('bounds')
-    if bounds:
-        return jsonify({"bounds": bounds})
+    if request.method == 'POST':
+        bounds = request.get_json().get('bounds')
+        if bounds:
+            return jsonify({"bounds": bounds})
+        else:
+            return jsonify({"message": "No bounds found in the request."})
     else:
-        return jsonify({"message": "No bounds found in the request."})
+        return jsonify({"message": "GET desCartes!"})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
