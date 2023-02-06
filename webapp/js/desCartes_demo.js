e2a67b5a814e8647141a68f0715a9d067cab8cd1
Stephen Gadd <42514781+docuracy@users.noreply.github.com>
2023-02-06 00:03:54 +0000

    Update desCartes_demo.js

--- webapp/js/desCartes_demo.js
+++ webapp/js/desCartes_demo.js
@@ -43,6 +43,8 @@ $(document).ready(function() {
                     maxZoom: 25,
                     crs: L.CRS.Simple
                 }).setView([0, 0], 1);
+				var bounds = L.latLngBounds([[0, 0], [image.height, image.width]]);
+				imageMap.fitBounds(bounds);
 
                 // Add the image to the map
                 L.imageOverlay(image.src, [
