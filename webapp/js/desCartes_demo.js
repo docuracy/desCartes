634612ce7e8bdd1e418e548e3380e8746fc6fc20
Stephen Gadd <42514781+docuracy@users.noreply.github.com>
2023-02-18 14:46:59 +0000

    Update desCartes_demo.js

--- webapp/js/desCartes_demo.js
+++ webapp/js/desCartes_demo.js
@@ -77,7 +77,7 @@ function showCandidateLines(response) {
 	$("<span class='comment'>To improve results, you may need to adjust the parameters before processing.</span>").appendTo(thumbnailsContainer);
 	var downloadButton = $("<button class='download'>").text("Download GeoPackage").appendTo(thumbnailsContainer);
 	downloadButton.click(function() {
-	    var binary = atob(response.file);
+	    var binary = atob(response.GeoPackage.gpkg);
 	    var byteArray = new Uint8Array(binary.length);
 	    for (var i = 0; i < binary.length; i++) {
 	        byteArray[i] = binary.charCodeAt(i);
