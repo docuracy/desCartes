9e7e8dac999ccce503258ecfb2c3700ce782957f
docuracy <42514781+docuracy@users.noreply.github.com>
2023-04-16 15:22:27 +0100

    -

--- colab_setup.py
+++ colab_setup.py
@@ -9,10 +9,12 @@ subprocess.run(['conda', 'install', '-q', '-c', 'ilastik-forge', 'vigra', 'z5py'
 if not os.path.exists('./data'):
     os.makedirs('./data')
 
-subprocess.run(['wget', '-q', 'https://descartes.docuracy.co.uk/ilastik/desCartes_preprocess.ilp', '-P', './data'])
+# subprocess.run(['wget', '-q', 'https://descartes.docuracy.co.uk/ilastik/desCartes_preprocess.ilp', '-P', './data'])
+subprocess.run(['wget', '-q', 'https://descartes.docuracy.co.uk/ilastik/pixel_classification.ilp', '-P', './data'])
+subprocess.run(['wget', '-q', 'https://descartes.docuracy.co.uk/ilastik/boundary_segmentation.ilp', '-P', './data'])
+subprocess.run(['wget', '-q', 'https://descartes.docuracy.co.uk/ilastik/segmentation_classification.ilp', '-P', './data'])
 
 # Install ilastik
 subprocess.run(['wget', 'https://files.ilastik.org/ilastik-1.4.0-Linux.tar.bz2'])
 subprocess.run(['tar', 'xjf', 'ilastik-1.4.0-Linux.tar.bz2'])
 subprocess.run(['rm', './ilastik-1.4.0-Linux.tar.bz2'])
-sys.path.append('/content/ilastik-1.4.0-Linux/lib/python3.7/site-packages')
